017d2bcbeb760c3eb40cc7d09d18a8fca0a2e587
Sergio-Romero12 <100429116@alumnos.uc3m.es>
2021-12-11 21:18:04 +0100

    mapas a estrella y cambios en el sh con los nuevos mapas

--- ASTAR-calls.sh
+++ ASTAR-calls.sh
@@ -4,4 +4,7 @@ python aestrella.py ./ASTAR-tests mapa3 contenedores3 heuristica1
 python aestrella.py ./ASTAR-tests mapa3 contenedores4 heuristica2
 python aestrella.py ./ASTAR-tests mapa3 contenedores5 heuristica2
 python aestrella.py ./ASTAR-tests mapa3 contenedores3 heuristica2
+python aestrella.py ./ASTAR-tests mapa6 contenedores4 heuristica1
+python aestrella.py ./ASTAR-tests mapa7 contenedores3 heuristica1
+python aestrella.py ./ASTAR-tests mapa7 contenedores3 heuristica2
 python aestrella.py ./ASTAR-tests mapa5 contenedores3 heuristica1
