5d30af6dc617e7a1b8c60c276573cbacc061e0eb
100429019 <100429019@alumnos.uc3m.es>
2021-12-12 15:35:17 +0100

    ASTARStowage.sh y calls actualizado

--- parte-2/ASTAR-calls.sh
+++ parte-2/ASTAR-calls.sh
@@ -1,10 +1,10 @@
-python aestrella.py ./ASTAR-tests mapa3 contenedores4 heuristica1
-python aestrella.py ./ASTAR-tests mapa3 contenedores5 heuristica1
-python aestrella.py ./ASTAR-tests mapa3 contenedores3 heuristica1
-python aestrella.py ./ASTAR-tests mapa3 contenedores4 heuristica2
-python aestrella.py ./ASTAR-tests mapa3 contenedores5 heuristica2
-python aestrella.py ./ASTAR-tests mapa3 contenedores3 heuristica2
-python aestrella.py ./ASTAR-tests mapa6 contenedores4 heuristica1
-python aestrella.py ./ASTAR-tests mapa7 contenedores3 heuristica1
-python aestrella.py ./ASTAR-tests mapa7 contenedores3 heuristica2
-python aestrella.py ./ASTAR-tests mapa5 contenedores3 heuristica1
+./ASTARStowage.sh ./ASTAR-tests mapa3 contenedores4 heuristica1
+./ASTARStowage.sh ./ASTAR-tests mapa3 contenedores5 heuristica1
+./ASTARStowage.sh ./ASTAR-tests mapa3 contenedores3 heuristica1
+./ASTARStowage.sh ./ASTAR-tests mapa3 contenedores4 heuristica2
+./ASTARStowage.sh ./ASTAR-tests mapa3 contenedores5 heuristica2
+./ASTARStowage.sh ./ASTAR-tests mapa3 contenedores3 heuristica2
+./ASTARStowage.sh ./ASTAR-tests mapa6 contenedores4 heuristica1
+./ASTARStowage.sh ./ASTAR-tests mapa7 contenedores3 heuristica1
+./ASTARStowage.sh ./ASTAR-tests mapa7 contenedores3 heuristica2
+./ASTARStowage.sh ./ASTAR-tests mapa5 contenedores3 heuristica1
---
+++ parte-2/ASTARStowage.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python ASTARStowage.py $1 $2 $3 $4
